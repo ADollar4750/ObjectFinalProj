3d2ab74f1c5b493a6ced977e84f342b625f7d0b8
Alexander Dollar <alexander.dollar@students.snow.edu>
2023-11-28 09:23:03 -0700

    Setting up serializer and it's tests

--- DeckBuilder.Test/UnitTest1.cs
+++ DeckBuilder.Test/UnitTest1.cs
@@ -7,9 +7,24 @@ public class Tests
     {
     }
 
-    //[Test]
-    // public void Test1()
-    // {
-    //     Assert.Pass();
-    // }
+    [Test]
+    public void TestSerializer()
+    {
+        DeckBuilderApp test = new DeckBuilderApp();
+        JSonScribe scribe = new JSonScribe();
+
+        Creature expected = new Creature(){
+            Name = "Tim",
+            ManaCost = "0",
+            Legendary = true,
+            CreatureType = "human",
+            Ability = "Sacrifice Tim to destroy target token.",
+            Power = 0,
+            Toughness = 2,
+        };
+        test.AddCard(expected);
+        scribe.deckSerizalizer(test.Deck);
+
+        Assert.AreEqual("Tim", );
+    }
 }
--- DeckBuilder/JsonScribe.cs
+++ DeckBuilder/JsonScribe.cs
@@ -1,4 +1,12 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text.Json;
+
 public class JSonScribe
 {
-    
+    public void deckSerizalizer(List<Card> deck)
+    {
+        string deckJson = JsonSerializer.Serialize(deck);
+        Console.WriteLine(deckJson);
+    }
 }
